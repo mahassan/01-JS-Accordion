09c3f327fa2e19860b0fdb2da3856ef123c63e93
Ammar Hassan <ammar@ammarhassan.co.uk>
2023-12-17 17:17:53 +0000

    hide when clicked

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log('hello world')
+const headers = document.querySelectorAll(".accordion-header");
+[...headers].forEach((header)=>{
+    //console.dir(header);
+    if(header.nextElementSibling.classList.contains("content")){
+        const content = header.nextElementSibling;
+        content.classList.add("hide");
+    }
+    //add a event listener
+    header.addEventListener("click", function(e){
+       // console.log(e.target.classList,' its is here')
+        if(e.target.classList.contains("accordion-header")){
+            console.log(e.target.nextElementSibling.classList.remove('hide'));
+        }
+    })
+})
