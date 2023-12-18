c45d034df324193e2e7515431ef5004434d202f6
Ammar Hassan <ammar@ammarhassan.co.uk>
2023-12-18 21:11:25 +0000

    update t html element and added aria

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const buttons = document.querySelectorAll(".accordion-button");
 [...buttons].forEach((button)=>{
     //console.dir(button);
-    const content = button.nextElementSibling.nextElementSibling;
-    if(content.classList.contains("content")){
-        content.classList.add("hide");
+    const disclosure = button
+    console.dir(disclosure);
+    if(disclosure.classList.contains("content")){
+        disclosure.classList.add("hide");
     }
     //add a event listener
     button.addEventListener("click", function(e){
@@ -18,7 +19,7 @@ const buttons = document.querySelectorAll(".accordion-button");
         if(e.target.classList.contains("accordion-button")){
             e.target.nextElementSibling.nextElementSibling.classList.toggle('hide');
             if(e.target.nextElementSibling.nextElementSibling.contains('hide')){
-                
+                console.dir(e.target);
             }
         }
     })
