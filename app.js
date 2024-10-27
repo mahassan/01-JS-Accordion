e838ea292095ceafa27c902125322c461c701eb2
Ammar Hassan <ammar@ammarhassan.co.uk>
2024-10-27 22:06:44 +0000

    demostration work, need styling and content

--- app.js
+++ app.js
@@ -1,26 +1,11 @@
-const buttons = document.querySelectorAll(".accordion-button");
-[...buttons].forEach((button)=>{
-    //console.dir(button);
-    const disclosure = button
-    console.dir(disclosure);
-    if(disclosure.classList.contains("content")){
-        disclosure.classList.add("hide");
-    }
-    //add a event listener
-    button.addEventListener("click", function(e){
-        //if the target clicked is not
-        // if(e.target !== e.target){
-        //     console.log(e.target);
-        // }
-        //not equal to target to the target clicked
-        //and have no hide class, put hide class
+const ac = document.querySelectorAll(".accordion");
 
-
-        if(e.target.classList.contains("accordion-button")){
-            e.target.nextElementSibling.nextElementSibling.classList.toggle('hide');
-            if(e.target.nextElementSibling.nextElementSibling.contains('hide')){
-                console.dir(e.target);
-            }
-        }
-    })
+ac.forEach((x,i) => {
+  x.addEventListener("click", function(e){
+  if(e.currentTarget.classList.contains('open')){
+    e.currentTarget.classList.remove('open')
+  }else{
+    e.currentTarget.classList.add('open')
+  }
+  })
 })
